f0ac190008968389a44ceae825bba2466a2a9363
wiseman88 <wiseadam02@gmail.com>
2022-10-04 13:43:10 +0200

    font + colors set up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'orange': 'hsl(26, 100%, 55%)',
+      'pale-orange': 'hsl(25, 100%, 94%)',
+      'white': '#ffffff',
+      'black': '#000',
+      'very-dark-blue': 'hsl(220, 13%, 13%)',
+      'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+      'grayish-blue': 'hsl(220, 14%, 75%)',
+      'light-grayish-blue': 'hsl(223, 64%, 98%)',
+    },
+    fontFamily: {
+      'body': ['Kumbh Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }
