984778713f6561c92dd9993fed0a50dce5946c7c
wiseman88 <wiseadam02@gmail.com>
2022-10-20 09:12:21 +0200

    ProductCarousel.vue css styles apply

--- src/components/ProductCarousel.vue
+++ src/components/ProductCarousel.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="products-slider-wrapper relative md:hidden">
-        <Carousel ref="carousel" v-model="currentSlide">
-            <Slide v-for="slide in images" :key="slide">
-                <img :src="slide.image" alt="product_1" class="object-cover w-full h-[300px]" />
-            </Slide>
-        </Carousel>
-        <div>
-            <button class="absolute top-1/2 left-4 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white"
-                @click="currentSlide--">
-                <figure>
-                    <img class="mx-auto mr-[16px] w-[9px]" src="../assets/images/icon-previous.svg" alt="prev">
-                </figure>
-            </button>
-            <button class="absolute top-1/2 right-4 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white"
-                @click="currentSlide++">
-                <figure>
-                    <img class="mx-auto ml-[16px] w-[9px]" src="../assets/images/icon-next.svg" alt="next">
-                </figure>
-            </button>
+    <div class="md:ml-12 md:mr-[102px]">
+        <div class="products-slider-wrapper relative md:hidden">
+            <Carousel ref="carousel" v-model="currentSlide">
+                <Slide v-for="slide in images" :key="slide">
+                    <img :src="slide.image" alt="product_1" class="object-cover w-full h-[300px]" />
+                </Slide>
+            </Carousel>
+            <div>
+                <button class="absolute top-1/2 left-4 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white"
+                    @click="currentSlide--">
+                    <figure>
+                        <img class="mx-auto mr-[16px] w-[9px]" src="../assets/images/icon-previous.svg" alt="prev">
+                    </figure>
+                </button>
+                <button class="absolute top-1/2 right-4 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white"
+                    @click="currentSlide++">
+                    <figure>
+                        <img class="mx-auto ml-[16px] w-[9px]" src="../assets/images/icon-next.svg" alt="next">
+                    </figure>
+                </button>
+            </div>
         </div>
-    </div>
-    <div class="hidden md:block">
-        <a href="#" class="block w-[445px] mb-[30px]">
-            <figure>
-                <img :src="mainImageUrl" alt="" class="rounded-[14px]">
-            </figure>
-        </a>
-        <div class="product-images-navigation flex justify-between">
-            <button v-for="(item, index) in images" :key="index"
-                class="product-button group w-[92px] rounded-[10px] overflow-hidden" @click="setImageUrl($event)">
+        <div class="hidden md:block">
+            <a href="#" class="block w-[445px] mb-[30px]">
                 <figure>
-                    <img :src="item.image" :alt="index" class="product-image" :class="{ 'active': index === 0 }">
+                    <img :src="mainImageUrl" alt="" class="rounded-[14px]">
                 </figure>
-            </button>
+            </a>
+            <div class="product-images-navigation flex justify-between">
+                <button v-for="(item, index) in images" :key="index"
+                    class="product-button group w-[92px] rounded-[10px] overflow-hidden" @click="setImageUrl($event)">
+                    <figure>
+                        <img :src="item.image" :alt="index" class="product-image" :class="{ 'active': index === 0 }">
+                    </figure>
+                </button>
+            </div>
         </div>
     </div>
 </template>
