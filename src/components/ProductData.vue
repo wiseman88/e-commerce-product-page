86494471da3b775ff4cf31dc03c86aaf14f8e2b1
wiseman88 <wiseadam02@gmail.com>
2022-10-22 12:17:39 +0200

    refactor: refactor svg.vue

--- src/components/ProductData.vue
+++ src/components/ProductData.vue
@@ -36,7 +36,11 @@
             </div>
             <Button @click="checkProductCount">
                 <figure class="mr-4">
-                    <SvgCart :fillColor="fillColor" />
+                    <Svg :width="22" :height="20">
+                        <path
+                            d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                            :fill="fillColor" fill-rule="nonzero" />
+                    </Svg>
                 </figure>
                 <span> Add to cart </span>
             </Button>
@@ -47,7 +51,7 @@
 <script setup>
 import { useProductStore } from "../stores/product";
 import { useCounterStore } from "../stores/counter";
-import SvgCart from "./SvgCart.vue";
+import Svg from "./Svg.vue";
 import Button from "./Button.vue";
 
 let fillColor = '#fff';
---
+++ src/components/Svg.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg :width="width" :height="height" xmlns="http://www.w3.org/2000/svg">
+        <slot></slot>
+    </svg>
+</template>
+
+<script setup>
+
+// const fillColor = "#69707D";
+
+const props = defineProps({
+    fillColor: String,
+    width: Number,
+    height: Number
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
